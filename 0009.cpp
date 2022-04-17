1c84824b972d6ced326c3e9aee8b4a75bcc6ff4a
baxro1 <59386817+baxro1@users.noreply.github.com>
2022-04-16 19:43:52 -0700

    increment and decrement operator
    we have tow example
    follow me

---
+++ 0009.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    
+    int a = 1; 
+    cout  << ++a << " => " << a << endl;
+    
+    int b = 1;
+    while(b <= 10) cout  << ++b << endl;
+    
+
+    return 0;
+}
